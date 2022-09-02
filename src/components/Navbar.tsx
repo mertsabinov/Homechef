411bd0aa49cdfe744c0e1d4cc063f588c18419f5
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-02 15:55:22 +0200

    add(Navbar): responsive hamburher menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,49 @@
 import * as react from 'react'
 import '../assets/styles/components/Navbar.css'
 import logo from '../assets/images/logo.svg'
-
+import {useState} from 'react'
 const Navbar = () => {
+    const [active,setActive] = useState<boolean>(false)
+
+    const activeHandler = (active:boolean) => {
+        if (active){
+            setActive(false)
+        } else {
+            setActive(true)
+        }
+    }
+
     return (
-        <div className='navbar'>
-            <div className='navbar-left-side'>
-                <img src={logo} alt='logo' />
+        <div>
+            <div
+                className={`${active ? "hamburger-menu-open":"hamburger-menu"}`}
+                onClick={() => activeHandler(active)}
+            >
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
-            <div className='navbar-center-side' >
-                <a href='#shop'>Shop</a>
-                <a href='#features'>Features</a>
-                <a href='#recipes'>Recipes</a>
-                <a href='#hotline' >Hotline</a>
-            </div>
-            <div className='navbar-right-side' >
-                <button id='btn-login'>
-                    Login
-                </button>
-                <button id='btn-sign-up'>
-                    Sign Up
-                </button>
+            <div className={`${active ? "navbar-open":"navbar"}`}>
+                <div className={`${active ? "navbar-left-side-open":"navbar-left-side"}`}>
+                    <img src={logo} alt='logo' />
+                </div>
+                <div className={`${active ? "navbar-center-side-open":"navbar-center-side"}`}>
+                    <a href='#shop'>Shop</a>
+                    <a href='#features'>Features</a>
+                    <a href='#recipes'>Recipes</a>
+                    <a href='#hotline' >Hotline</a>
+                </div>
+                <div className={`${active ? "navbar-right-side-open":"navbar-right-side"}`}>
+                    <button id='btn-login'>
+                        Login
+                    </button>
+                    <button id='btn-sign-up'>
+                        Sign Up
+                    </button>
+                </div>
             </div>
         </div>
+
     )
 }
 
